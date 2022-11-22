fed18590afab9a07698f58d771ae2f8d4783a4d5
pricellvalencia <pricellvalencia2222@gmail.com>
2022-11-22 21:02:57 +0700

    bagian Pricell 200710703

--- app/src/main/java/com/example/tubes_kelompok10/api/LowonganApi.kt
+++ app/src/main/java/com/example/tubes_kelompok10/api/LowonganApi.kt
@@ -2,7 +2,7 @@ package com.example.tubes_kelompok10.api
 
 class LowonganApi {
     companion object {
-        val BASE_URL = "https://637ca48272f3ce38eaa914d5.mockapi.io/"
+        val BASE_URL = "http://192.168.239.15/api/lowongan"
 
         val GET_ALL_URL = BASE_URL + "lowongan/"
         val GET_BY_ID_URL = BASE_URL + "lowongan/"
