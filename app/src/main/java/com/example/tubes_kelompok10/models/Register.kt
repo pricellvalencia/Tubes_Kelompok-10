c7466fc0db4c62dab378b8f2c0e6d35fbe20e06e
SetoHer <seto.herlambang.23@gmail.com>
2022-12-18 18:52:14 +0700

    model register

---
+++ app/src/main/java/com/example/tubes_kelompok10/models/Register.kt
@@ -0,0 +1,5 @@
+package com.example.tubes_kelompok10.models
+
+class Register (var username: String, var email: String, var noPhone: String, var tglLahir: String, var password: String ) {
+    var id: Long? = null
+}
